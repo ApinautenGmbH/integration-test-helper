da53427ad078c65f1c5ce9d9acd409de07597715
Philipp Gillé <gille@apiomat.com>
2017-09-11 17:29:57 +0200

    Add method for fetching MetaModels

--- src/main/java/com/apiomat/helper/AomHttpClient.java
+++ src/main/java/com/apiomat/helper/AomHttpClient.java
@@ -1059,7 +1059,7 @@ public class AomHttpClient
 		}
 		return request;
 	}
-	
+
 	/**
 	 * delete static data, either image or file
 	 *
@@ -1070,7 +1070,8 @@ public class AomHttpClient
 	public HttpMethod deleteStaticData( String id, final boolean isImage )
 	{
 		final DeleteMethod request =
-			new DeleteMethod( this.yambasBase + "apps/" + this.appName + "/data/" + ( isImage ? "images/" : "files/" ) + id );
+			new DeleteMethod(
+				this.yambasBase + "apps/" + this.appName + "/data/" + ( isImage ? "images/" : "files/" ) + id );
 		request.setRequestHeader( "Content-Type", "application/octet-stream" );
 		request.setRequestHeader( "x-apiomat-apikey", this.apiKey );
 		request.setRequestHeader( "x-apiomat-system", this.system.toString( ) );
@@ -1437,6 +1438,30 @@ public class AomHttpClient
 		}
 
 		return result;
+	}
+
+	/**
+	 * Get all MetaModels (classes) of a module
+	 *
+	 * @param moduleName
+	 * @return the {@link HttpMethod} object after executing the request
+	 */
+	public HttpMethod getMetaModels( final String moduleName )
+	{
+		GetMethod request = new GetMethod( this.yambasBase + "modules/" + moduleName + "/metamodels" );
+		setAuthorizationHeader( request );
+		request.setRequestHeader( "ContentType", "application/json" );
+		request.setRequestHeader( "x-apiomat-system", this.system.toString( ) );
+		request.setRequestHeader( "x-apiomat-sdkVersion", "1.0" );
+		try
+		{
+			this.client.executeMethod( request );
+		}
+		catch ( IOException e )
+		{
+			e.printStackTrace( );
+		}
+		return request;
 	}
 
 	private void setAuthorizationHeader( HttpMethodBase requestMethod )
